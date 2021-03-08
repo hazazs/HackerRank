a4f86b203bed0b4c5f41a7d9e7ae456edf5236a9
hazazs <hazafi.tibor@gmail.com>
2021-03-08 11:19:30 +0100

    init

---
+++ Interview Preparation Kit/WarmUpChallenges.java
@@ -0,0 +1,25 @@
+
+    /* Sales by Match */
+    static int sockMerchant(int n, int[] ar) {
+        Map<Integer, Integer> socks = new HashMap<>();
+        for (int i : ar)
+            socks.put(i, socks.get(i) == null ? 1 : socks.get(i)+1);
+        int numberOfPairs = 0;
+        for (int i : socks.values())
+            numberOfPairs += i/2;
+        return numberOfPairs;
+        // return socks.values().stream().mapToInt(i -> i/2).sum();
+        // return socks.values().stream().collect(Collectors.summingInt(i -> i/2));
+    }
+    
+    /* Counting Valleys */
+    public static int countingValleys(int steps, String path) {
+        int numberOfValleys = 0;
+        int altitude = 0;
+        for (int i = 0; i < steps; i++) {
+            altitude += path.charAt(i) == 'U' ? 1 : -1;
+            if (altitude == 0 && path.charAt(i) == 'U')
+                numberOfValleys++;
+        }
+        return numberOfValleys;
+    }
