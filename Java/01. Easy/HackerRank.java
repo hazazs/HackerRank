b227dc434de46a15127091f9099c20d67d09d719
hazazs <hazafi.tibor@gmail.com>
2021-04-16 15:43:29 +0200

    HackerRank

--- Java/01. Easy/HackerRank.java
+++ Java/01. Easy/HackerRank.java
@@ -39,4 +39,63 @@ public class HackerRank {
         scan.close();
     }
     
+    /*
+    Java Loops I
+     */
+    public void javaLoopsI() {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for (int i = 1; i <= 10; i++)
+            System.out.println(N + " x " + i + " = " + N*i);
+        scanner.close();
+    }
+    
+    /*
+    Java Loops II
+     */
+    public void javaLoopsII() {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            for (int j = 0; j < n; j++) {
+                a += (int) Math.pow(2, j) * b;
+                System.out.print(a + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+    
+    /*
+    Java If-Else
+     */
+    public void javaIfElse() {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if (N%2 == 0)
+            System.out.println(N >= 2 && N <= 5 || N > 20 ? "Not Weird" : "Weird");
+        else System.out.println("Weird");
+        scanner.close();
+    }
+    
+    /*
+    Java Output Formatting
+     */
+    public void javaOutputFormatting() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================================");
+        for (int i = 0; i < 3; i++) {
+            String s1 = sc.next();
+            int x = sc.nextInt();
+            System.out.printf("%-15.15s%03d%n", s1, x);
+        }
+        System.out.println("================================");
+        sc.close();
+    }
+    
 }
