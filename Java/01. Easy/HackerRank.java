d7c55383049a44f9d54546d304539f08ce735e9c
hazazs <hazafi.tibor@gmail.com>
2021-04-20 16:22:20 +0200

    HackerRank

--- Java/01. Easy/HackerRank.java
+++ Java/01. Easy/HackerRank.java
@@ -98,4 +98,60 @@ public class HackerRank {
         sc.close();
     }
     
+    /*
+    Java Datatypes
+     */
+    public void javaDatatypes() {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++)
+            try {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+                if (x >= -128 && x <= 127)
+                    System.out.println("* byte");
+                if (x >= -1 * Math.pow(2, 15) && x <= Math.pow(2, 15) - 1)
+                    System.out.println("* short");
+                if (x >= -1 * Math.pow(2, 31) && x <= Math.pow(2, 31) - 1)
+                    System.out.println("* int");
+                if (x >= -1 * Math.pow(2, 63) && x <= Math.pow(2, 63) - 1)
+                    System.out.println("* long");
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+    }
+    
+    /*
+    Java End-of-file
+     */
+    public void javaEndOfFile() {
+        Scanner scanner = new Scanner(System.in);
+        int counter = 1;
+        while (scanner.hasNext())
+            System.out.println(counter++ + " " + scanner.nextLine());
+    }
+    
+    /*
+    Java Static Initializer Block
+     */
+    private final static Scanner scanner = new Scanner(System.in);
+    private final static int B = scanner.nextInt();
+    private final static int H = scanner.nextInt();
+    private final static boolean flag = B > 0 && H > 0;
+
+    static {
+        if (!flag)
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+    }
+    
+    /*
+    Java Int to String
+     */
+    public void javaIntToString() {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.close();
+        String s = String.valueOf(n);
+    }
+    
 }
